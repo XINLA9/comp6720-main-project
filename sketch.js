76a3a61085e352b72137c5867ce1cd8e53bd9d1d
xinhe <u7518549@anu.edu.au>
2024-10-14 22:44:44 +1100

    Basic frame

--- sketch.js
+++ sketch.js
@@ -21,3 +21,46 @@ function keyTyped() {
     saveCanvas("thumbnail.png");
   }
 }
+
+function drawRails() {
+  stroke(255); 
+  strokeWeight(10); 
+
+
+  for (let i = 0; i < 20; i++) {
+    let progress = (i + railOffset) % 50; 
+
+    let y = map(progress, 0, 50, height, height / 2);
+
+    let leftX = map(progress, 0, 50, width * 0.25, width / 2);
+    let rightX = map(progress, 0, 50, width * 0.75, width / 2);
+
+    line(leftX, y, rightX, y);
+  }
+
+  strokeWeight(5); 
+  line(width * 0.1, height, width / 2, height / 2); 
+  line(width * 0.9, height, width / 2, height / 2); 
+
+  railOffset += speed * 0.1;
+}
+
+function drawDessert(){
+  
+};
+
+function drawForest(){
+  
+};
+
+function drawCity(){
+  
+};
+
+function drawSea(){
+  
+};
+
+function drawSpace(){
+  
+};
