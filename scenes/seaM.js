3fc03bbd0c2a30db28c9a5b2669ba5bc257a67e4
Xinhe Lai <u7518549@anu.edu.au>
2024-10-30 07:18:20 +1100

    Add sea shrine scene

--- scenes/seaM.js
+++ scenes/seaM.js
@@ -1,162 +1,178 @@
-let seaTower = [];
+let toriis = [];
+let seaCount = 0;
 
 function drawSeaM() {
-  background(31, 94, 139);
+  background(15, 58, 84);
   push();
   noStroke();
-  fill(color(132, 181, 201));
+  fill("#607D8B");
   rect(0, 0.5 * height, width, 0.5 * height);
   pop();
 
-  if (!playable_whole && count_whole <= 10) {
-    count_whole += 1;
-  }
+  drawShrine();
+  drawTorii();
 
-  if (count_whole > 10) {
-    count_whole = 0;
-    playable_whole = true;
+  if ((seaCount > 600) & !portal.active) {
+    portal.open("space");
   }
-  // display all item
-
-  drawSeaSceneM();
 }
 
-function drawSeaSceneM() {
-  if (seaTower.length < 50) {
-    if (countNotes[0] > 3) {
-      seaTower.push(new SeaWeed(0));
-      sceneCount[0]++;
-      countNotes[0] = 0;
-    }
-    if (countNotes[1] > 3) {
-      seaTower.push(new SeaWeed(1));
-      sceneCount[1]++;
-      countNotes[1] = 0;
-    }
-    if (countNotes[2] > 3) {
-      seaTower.push(new SeaWeed(2));
-      sceneCount[2]++;
-      countNotes[2] = 0;
-    }
-  }
-  if(!portal.active){
-    if (sceneCount[0] > 50) {
-       portal.open("desert");
-       sceneCount = [0,0,0];
-    }
-    if (sceneCount[1] > 50) {
-       portal.open("forest");
-    }
-    if (sceneCount[2] > 50) {
-       portal.open("space");
-    }
-  }
-
-  // Draw seaitems
+function drawShrine() {
   push();
-  rectMode(CENTER);
-  translate(width / 2, height / 2);
-  for (let i = seaTower.length - 1; i >= 0; i--) {
-    seaTower[i].update();
-    seaTower[i].show();
+  // Draw road
+  fill("#9E9E9E");
+  quad(
+    width * -0.15,
+    height,
+    width * 1.15,
+    height,
+    width * 0.52,
+    height / 2,
+    width * 0.48,
+    height / 2
+  );
 
-    if (seaTower[i].z < 0) {
-      seaTower.splice(i, 1);
-    }
-  }
+  // Draw shrine
+  fill("rgb(255,255,255)");
+  beginShape();
+  
+  vertex(width * 0.32, height * 0.32);
+  vertex(width * 0.31, height * 0.5);
+  vertex(width * 0.45, height * 0.5);
+  vertex(width * 0.45, height * 0.4);
+  vertex(width * 0.55, height * 0.4);
+  vertex(width * 0.55, height * 0.5);
+  vertex(width * 0.69, height * 0.5);
+  vertex(width * 0.68, height * 0.32);
+  
+  endShape(CLOSE);
+
+  fill("#F44336");
+  beginShape();
+  vertex(width * 0.29, height * 0.34);
+  vertex(width * 0.29, height * 0.3);
+  vertex(width * 0.71, height * 0.3);
+  vertex(width * 0.71, height * 0.34);
+  endShape(CLOSE);
+
+  fill("#343232");
+  beginShape();
+  vertex(width * 0.33, height * 0.3);
+  vertex(width * 0.33, height * 0.2);
+  vertex(width * 0.67, height * 0.2);
+  vertex(width * 0.67, height * 0.3);
+  endShape(CLOSE);
+
+  fill("#FF0000");
+  beginShape();
+  vertex(width * 0.15, height * 0.17);
+  vertex(width * 0.33, height * 0.2);
+  vertex(width * 0.67, height * 0.2);
+  vertex(width * 0.85, height * 0.17);
+  endShape(CLOSE);
+  
+  fill("#6C6B6B");
+  beginShape();
+  vertex(width * 0.15, height * 0.17);
+  vertex(width * 0.31, height * 0.11);
+  vertex(width * 0.33, height * 0.03);
+  vertex(width * 0.67, height * 0.03);
+  vertex(width * 0.69, height * 0.11);
+  vertex(width * 0.85, height * 0.17);
+  endShape(CLOSE);
+
+  
+
+  fill("rgb(239,199,41)");
+  beginShape();
+
+  endShape(CLOSE);
 
   pop();
 }
 
-class SeaTower {
-  constructor(no) {
-    let side = random(["left", "right"]);
-    do {
-      if (side == "left") {
-        this.x = random(-0.5 * width, 0);
-      } else {
-        this.x = random(0, 0.5 * width);
-      }
-    } while (this.x > -0.1 * width && this.x < 0.1 * width);
+function drawTorii() {
+  push();
 
-    this.y = random(height * 0.05, height * 0.1);
-    this.z = width + height;
-    this.side = "side";
+  seaCount++;
 
-    let types = ["sand", "star", "leaf"];
-
-    this.type = types[no];
-
-    this.color = this.getColorBasedOnType();
+  if (seaCount % 60 == 0) {
+    toriis.push(new Torii());
   }
 
-  getColorBasedOnType() {
-    switch (this.type) {
-      case "sand":
-        return random([color("#FF9800"), color("#FF5722")]);
-      case "leaf":
-        return random([color("#8BC34A"), color("#CDDC39")]);
-      case "star":
-        return random([color("#9E9E9E"), color("#00BCD4")]);
+  for (let i = toriis.length - 1; i >= 0; i--) {
+    toriis[i].update();
+    toriis[i].show();
+
+    if (toriis[i].z < 0) {
+      toriis.splice(i, 1);
+      toriis.push(new Torii());
     }
+  }
+  pop();
+}
+
+class Torii {
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.x = width / 2;
+    this.y = height / 2;
   }
 
   update() {
-    this.z -= speed;
-    this.sx = map(this.x / this.z, 0, 1, 0, width);
-    this.sy = map(this.y / this.z, 0, 1, 0, height);
-    this.size = map(this.z, 0, width + height, width * 0.2, 0);
-
-    this.thick = map(this.z, 0, width + height, 15, 0);
+    this.speed = getYSpeed(this.y);
+    this.y += this.speed;
   }
 
   show() {
     push();
-
-    stroke(34, 139, 34);
-    strokeWeight(this.thick);
-    noFill();
-
-    beginShape();
-    curveVertex(this.sx, this.sy);
-    curveVertex(this.sx, this.sy);
-    curveVertex(this.sx + random(-20, 20), this.sy - this.size * 0.5);
-    curveVertex(this.sx + random(-30, 30), this.sy - this.size * 1.5);
-    curveVertex(this.sx + random(-10, 10), this.sy - this.size * 2.5);
-    endShape();
-
-    let color = this.color;
-    color.setAlpha(200);
-    fill(color);
-
     noStroke();
+    let x = 1.2 * map(this.y, height / 2, height, 0, width);
+    let y = 0.9 * map(this.y, height / 2, height, 0, height);
 
-    triangle(
-      this.sx - this.size * 0.2,
-      this.sy - this.size * 0.5,
-      this.sx + this.size * 0.2,
-      this.sy - this.size * 0.5,
-      this.sx + random(-5, 5),
-      this.sy - this.size * 1
-    );
+    translate(0, -height * 0.02);
+    fill("red");
+    noStroke();
+    rect(0.5 * width - 0.8 * x, 0.5 * height - y, x * 0.2, y * 2);
+    rect(0.5 * width + 0.8 * x, 0.5 * height - y, -x * 0.2, y * 2);
+    rect(0.5 * width + x * 0.1, 0.5 * height - y, -x * 0.2, y * 0.7);
+    fill("black");
+    rect(0.5 * width - 0.8 * x, 0.5 * height + 0.5 * y, x * 0.2, y * 0.5);
+    rect(0.5 * width + 0.8 * x, 0.5 * height + 0.5 * y, -x * 0.2, y * 0.5);
+    fill("red");
+    rect(0.5 * width - x, 0.5 * height - y, x * 2, y * 0.2);
+    rect(0.5 * width - x, 0.5 * height - y * 0.5, x * 2, y * 0.2);
+    fill("black");
+    rect(0.5 * width - x * 1.2, 0.5 * height - y * 1.15, x * 2.4, y * 0.15);
 
-    triangle(
-      this.sx - this.size * 0.3,
-      this.sy - this.size * 1.2,
-      this.sx + this.size * 0.3,
-      this.sy - this.size * 1.2,
-      this.sx + random(-10, 10),
-      this.sy - this.size * 1.7
-    );
-
-    triangle(
-      this.sx - this.size * 0.25,
-      this.sy - this.size * 1.8,
-      this.sx + this.size * 0.25,
-      this.sy - this.size * 1.8,
-      this.sx + random(-5, 5),
-      this.sy - this.size * 2.3
-    );
     pop();
+  }
+
+  open(s) {
+    if (s) {
+      this.scene = s;
+      this.color = this.getColorBasedOnScene();
+      this.active = true;
+    }
+  }
+
+  getColorBasedOnScene() {
+    switch (this.scene) {
+      case "forest":
+        return "#BEE393";
+      case "desert":
+        return "#F3CD96";
+      case "sea":
+        return "#87B0B5";
+      case "space":
+        return "#AFAFAF";
+      case "forestM":
+        return [0, 105, 148];
+      default:
+        return [0, 0, 0];
+    }
   }
 }
